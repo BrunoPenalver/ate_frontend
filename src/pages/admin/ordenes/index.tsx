16a9fc96b069f7897ee98cfecc0edf66d480221b
Bruno Peñalver <brunonicolaspenalver@gmail.com>
2024-07-12 01:49:44 -0300

    fix rutas publicas

--- src/pages/admin/ordenes/index.tsx
+++ src/pages/admin/ordenes/index.tsx
@@ -1,9 +1,12 @@
 import { ConfirmDialog } from "primereact/confirmdialog"
 import { StyledTable } from "../../../components/tables/orders-table/OrdersTable"
 import AdminLayout from "../../../layouts/Admin"
+import useAuth from "../../../hooks/auth"
 
 const OrdenesActivasPage = () => 
 {
+  useAuth();
+
   return (
     <>
     <AdminLayout>
