d334b6d24d4954bcffdc9ba2be98428e62f53b0d
Bruno Peñalver <brunonicolaspenalver@gmail.com>
2024-08-23 17:58:53 -0300

    cambios para reunion del 23/8

---
+++ cypress/e2e/ordenes/copiar_rotar.cy.ts
@@ -0,0 +1,33 @@
+import { openAndFillMovimientoForm } from "../utils/agregar";
+import { correctLogin } from "../utils/login";
+import "cypress-plugin-tab";
+
+describe('Crear Orden', () =>
+{
+    it('Copiar', () => 
+    {
+        correctLogin("admin", "ZwyiHTqljvRn");
+        cy.visit("/admin/ordenes/agregar");
+      
+        cy.get("#date").click();
+        cy.get(".p-datepicker-today").click();
+        cy.get("#description").type("Descripción de prueba");
+
+        openAndFillMovimientoForm(false);
+        
+        cy.get("i.pi.pi-copy").click();
+    });
+
+    it('Rotar', () => 
+    {
+        correctLogin("admin", "ZwyiHTqljvRn");
+        cy.visit("/admin/ordenes/agregar");
+        
+        cy.get("#date").click();
+        cy.get(".p-datepicker-today").click();
+        cy.get("#description").type("Descripción de prueba");
+
+        openAndFillMovimientoForm(true);
+        cy.get("i.pi.pi-arrow-circle-left, i.pi.pi-arrow-circle-up").click();
+    });
+});
---
+++ cypress/e2e/ordenes/editar.cy.ts
@@ -0,0 +1,39 @@
+import { openAndFillMovimientoForm } from "../utils/agregar";
+import { correctLogin } from "../utils/login";
+import "cypress-plugin-tab";
+
+describe('Crear Orden', () =>
+{
+    it('Crear Orden sin movimientos', () => 
+    {
+        cy.viewport(1920, 1080);
+        correctLogin("admin", "ZwyiHTqljvRn");
+        cy.visit("/admin/ordenes/agregar");
+        
+        cy.get("#date").click();
+        cy.get("td.p-datepicker-today").click();
+        cy.get("#description").type("Descripción de prueba");
+
+        cy.get("#save-order").click();
+
+        cy.url().should('include', '/admin/ordenes/');
+    });
+
+    it('Crear Orden', () => 
+    {
+        correctLogin("admin", "ZwyiHTqljvRn");
+        cy.visit("/admin/ordenes/agregar");
+      
+        cy.get("#date").click();
+        cy.get(".p-datepicker-today").click();
+        cy.get("#description").type("Descripción de prueba");
+
+        openAndFillMovimientoForm(false);
+        openAndFillMovimientoForm(true);
+        openAndFillMovimientoForm(true);
+
+        cy.get("#save-order").click();
+
+        cy.url().should('include', '/admin/ordenes');
+    });
+});
