9aa65213387ed1ffd8f13bf5db7b18cb795d4424
Bruno Peñalver <brunonicolaspenalver@gmail.com>
2024-10-09 21:16:37 -0300

    git merge

--- src/components/admin/Orders/MovimientoCreateOrUpdateMovimiento.cy.tsx
+++
@@ -1,9 +0,0 @@
-
-import CreateOrUpdateMovimiento from './Movimiento'
-
-describe('<CreateOrUpdateMovimiento />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<CreateOrUpdateMovimiento />)
-  })
-})
